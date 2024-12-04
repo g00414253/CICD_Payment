8fd811b6b03cecc1418ba04e1919cf6cc22ea94f
g00414253 <g00414253@atu.ie>
2024-12-04 16:37:03 +0000

    Created project for payment service

---
+++ src/main/java/ie/atu/CICD_PaymentApplication.java
@@ -0,0 +1,11 @@
+package ie.atu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CICD_PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CICD_PaymentApplication.class, args);
+    }
+}
